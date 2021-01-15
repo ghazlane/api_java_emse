2ad4e37fd544c5491bdba95de1d0eb28f8a3fc97
Mohammed GHAZLANE <mohammed.ghazlane@etu.emse.fr>
2021-01-16 00:45:37 +0100

    Version 1

---
+++ src/main/java/com/ghazlane/projectweb/web_project_ghazlane/hello/DummyUserService.java
@@ -0,0 +1,31 @@
+package com.ghazlane.projectweb.web_project_ghazlane.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DummyUserService implements UserService {
+
+    GreetingService greetingService;
+
+    @Autowired
+    public DummyUserService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @Override
+    public void greetAll() {
+        List<String> listElement = new ArrayList<>();
+        listElement.add("Elodie");
+        listElement.add("Charles");
+
+        for (String element : listElement) {
+            this.greetingService.greet(element);
+        }
+    }
+}
